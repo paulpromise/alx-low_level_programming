4b8fadbf7e2e9619eb7ee4c92b1e0cd2d8add884
paulpromise <paulmensah394@gmail.com>
2023-03-16 13:59:50 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,8 +5,9 @@
  *
  * Return: Always 1 (Success)
  */
+int main(void)
 {
 	char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
        	write(2, msg, 52);
-	return 1
+	return (1);
 }
